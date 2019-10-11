0c9d44c83416086d18ca9a20ae3f03a326017903
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-10-10 21:04:41 -0500

    se añado conexion Mongo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const  bodyParser = require ('body-parser') 
+const mongoose = require('mongoose');
 const app = express()
-var  bodyParser = require ('body-parser') 
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -41,6 +43,15 @@ app.get('/producto/:id', function (req, res) {
   })
 })
  
+mongoose.connect('mongodb://localhost:27017/tienda', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, res) => {
+  if(err)throw err;
+  console.log("conectado a la DB");
+});
+
+
 app.listen(3000, () =>{
   console.log("servidor ONLINE");
 });
